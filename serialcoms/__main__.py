fa37c7a56dc46c479261a569dfb8f64c510473a0
Steven <texruska@users.noreply.github.com>
2020-03-17 22:43:26 +0000

    Public interface skeleton

---
+++ serialcoms/__main__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""SerialComs package"""
+
+from typing import Callable
+
+import serial
+
+class SerialComs:
+    def __init__(self, port: str, baud: int) -> None:
+        """Connect to serial port"""
+        self.serial = serial.Serial(port=port, baudrate=baud)
+
+    def sendln(self, line: str) -> None:
+        """Send line over port"""
+
+    def register_callback(self, func: Callable) -> None:
+        """Register callback to call when a line is received"""
